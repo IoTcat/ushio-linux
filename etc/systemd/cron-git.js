8dbe95be3c1e0fe02b66451d2c8b4ca663c13a6c
ushio-uk <ushio@uk.yimian.xyz>
2020-01-20 18:13:38 +0100

    Committed by cron-git from uk.yimian.xyz

--- etc/systemd/cron-git.js
+++ etc/systemd/cron-git.js
@@ -1,3 +1,6 @@
 const crongit = require('cron-git')({dir: __dirname + '/../../'});
 
-crongit.schedule("* */3 * * * *", ()=>console.log('sycn ok..'), ()=>console.log('sync err...'));
+//crongit.schedule("* */3 * * * *", ()=>console.log('sycn ok..'), ()=>console.log('sync err...'));
+setInterval(()=>{
+	crongit.sync();
+}, 1000*60*3);
