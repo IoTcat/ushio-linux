643d4c7601cbc6df73d3b619d9d65a23bb3ea9b6
iotcat <ushio@cn.yimian.xyz>
2020-01-20 23:35:33 +0800

    Committed by cron-git from ushio-cn

--- etc/init.d/chmod.sh
+++ etc/init.d/chmod.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd ~/
-chmod r
+chmod u=wrx -R ./
+chmod o= -R ./
+chmod g= -R ./
+chmod g=rx -R lib/
+chmod g=w -R var/log/
+chmod g=wrx -R var/tmp/
+chmod g=rx -R src/
+chmod g=rx -R bin/
