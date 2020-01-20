9aa8916096d4a079a38c12208b93d393eafd613d
iotcat <i@iotcat.me>
2020-01-20 16:35:16 +0000

    Committed by cron-git from Yimian-PC

--- etc/systemd.win/cron-git.js
+++ etc/systemd.win/cron-git.js
@@ -1,3 +1,5 @@
 const crongit = require('cron-git')({dir: __dirname+'\\..\\..\\'});
 
-crongit.schedule("* */3 * * * *", ()=>console.log('test ok!!!\nuse Ctrl+C to exit!!'), ()=>console.log('pull err err err err!!!!'));
+setInterval(()=>{
+	crongit.sync();
+}, 1000*60*3);
